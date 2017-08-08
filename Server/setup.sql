a1ce67d1c3a397d64d4d9f212e026eed7b238ce4
fujie <fujie.me@qq.com>
2017-08-08 22:26:20 +0800

    chat_room

--- Server/setup.sql
+++ Server/setup.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE chat_room CHARACTER SET utf8;
 
 CREATE TABLE user(
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '主键,自增',
+    uid int NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT '主键,自增',
     name char(30) COMMENT '用户名',
     password char(32) COMMENT 'MD5加密后的密码'
 );
